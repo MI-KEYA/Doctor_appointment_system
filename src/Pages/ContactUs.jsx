42dfadcca037bdc2a9b00d8c000dd7c58452aa13
MI-KEYA <moniakeya913@gmail.com>
2025-04-30 11:41:50 +0600

    little modifications

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router';
+
+import ErrorPage from './ErrorPage';
 
 const ContactUs = () => {
     return (
-        <div className='w-2/3 mx-auto text-center'>
-            <div className='flex justify-center mb-5'>
-                <img src="/src/assets/error404.jpeg" alt="" />
-            </div>
-            <h1 className='font-bold text-3xl text-pink-300 mb-5'>404 - Page Not Found</h1>
-            <p className='mb-5 font-semibold'>Oops! the page you're looking for doen't exist</p>
-            <div>
-                <Link to='/'><button className='btn mb-10 bg-blue-600 text-white rounded-3xl'>Go Back Home</button></Link>
-            </div>
-        </div>
+        
+        <ErrorPage></ErrorPage>
+        
     );
 };
 
--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>Error page not found</h1>
+        <div className='w-2/3 mx-auto text-center'>
+        <div className='flex justify-center mb-5'>
+            <img src="/src/assets/error404.jpeg" alt="" />
         </div>
+        <h1 className='font-bold text-3xl text-pink-300 mb-5'>404 - Page Not Found</h1>
+        <p className='mb-5 font-semibold'>Oops! the page you're looking for doen't exist</p>
+        <div>
+            <Link to='/'><button className='btn mb-10 bg-blue-600 text-white rounded-3xl'>Go Back Home</button></Link>
+        </div>
+    </div>
     );
 };
 
