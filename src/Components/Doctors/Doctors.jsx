8186257f03e8e98e02af1068593dc1188c3c6b35
MI-KEYA <moniakeya913@gmail.com>
2025-04-30 11:35:16 +0600

    added contact us page

--- src/Components/Doctors/Doctors.jsx
+++ src/Components/Doctors/Doctors.jsx
@@ -21,7 +21,7 @@ const Doctors = ({ data }) => {
             <p className='text-center text-gray-500 text-xl mb-10 px-5'>Our platform connects you with verified, experienced doctors across various specialties — all at your convenience.
                 Whether it's a routine checkup or urgent consultation, book appointments in minutes and receive quality care you can trust.</p>
             <div className='grid lg:grid-cols-3 grid-col-1 gap-3 '>
-                <Suspense fallback={<span>Loadingg......</span>}>
+                <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
                     {
                         displayDoctors.map((singleDoctor) => <Doctor key={singleDoctor.registration_number} singleDoctor={singleDoctor}></Doctor>)
                     }
--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const ContactUs = () => {
     return (
-        <div>
-            no contact available
+        <div className='w-2/3 mx-auto text-center'>
+            <div className='flex justify-center mb-5'>
+                <img src="/src/assets/error404.jpeg" alt="" />
+            </div>
+            <h1 className='font-bold text-3xl text-pink-300 mb-5'>404 - Page Not Found</h1>
+            <p className='mb-5 font-semibold'>Oops! the page you're looking for doen't exist</p>
+            <div>
+                <Link to='/'><button className='btn mb-10 bg-blue-600 text-white rounded-3xl'>Go Back Home</button></Link>
+            </div>
         </div>
     );
 };
